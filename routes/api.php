fd8f8d6440970e246191783eb4e6eef88ec683a2
AliGeranmayeh <aligeranmayeh9797@gmail.com>
2024-04-09 16:26:48 +0430

    add all the task related routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthenticationController;
+use App\Http\Controllers\Task\TaskController;
 
 
 
@@ -9,3 +10,11 @@ use App\Http\Controllers\Auth\AuthenticationController;
 Route::post('register', [AuthenticationController::class , 'register'])->name('register');
 Route::post('login', [AuthenticationController::class , 'login'])->name('login');
 Route::post('logout', [AuthenticationController::class , 'logout'])->name('logout')->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('tasks', [TaskController::class , 'index'])->name('tasks.index');
+    Route::post('tasks', [TaskController::class , 'store'])->name('tasks.store');
+    Route::patch('tasks/{task}', [TaskController::class , 'update'])->name('tasks.update');
+    Route::get('tasks/{task}', [TaskController::class , 'show'])->name('tasks.show');
+    Route::delete('tasks/{task}', [TaskController::class , 'delete'])->name('tasks.delete');
+});
