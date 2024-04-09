211f02ddd7b6dc7b16e7cb40ba37acd2d36d674d
AliGeranmayeh <aligeranmayeh9797@gmail.com>
2024-04-09 10:25:52 +0430

    create login form request

--- app/Http/Controllers/Auth/AuthenticationController.php
+++ app/Http/Controllers/Auth/AuthenticationController.php
@@ -7,6 +7,9 @@ use App\Http\Requests\Auth\RegisterRequest;
 use App\Models\User;
 use App\Helpers\DB\UserRepository;
 use App\Helpers\Responses\RegisterResponse;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Helpers\Responses\LoginResponse;
+use Illuminate\Support\Facades\Auth;
 
 class AuthenticationController extends Controller
 {
@@ -17,11 +20,10 @@ class AuthenticationController extends Controller
         return ($registerIsSuccess) ? 
             RegisterResponse::success() :
             RegisterResponse::failed();
-
     }
 
 
-    public function login()
+    public function login(LoginRequest $request)
     {
     # code...
     }
